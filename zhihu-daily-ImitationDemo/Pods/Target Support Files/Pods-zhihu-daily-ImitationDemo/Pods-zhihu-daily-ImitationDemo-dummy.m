e0fe02ff9529ab8079f71de175baecebe4370c27
jiangkaiwen <894912881@qq.com>
2018-10-19 11:00:53 +0800

    添加第三方库与MVC分开

---
+++ zhihu-daily-ImitationDemo/Pods/Target Support Files/Pods-zhihu-daily-ImitationDemo/Pods-zhihu-daily-ImitationDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_zhihu_daily_ImitationDemo : NSObject
+@end
+@implementation PodsDummy_Pods_zhihu_daily_ImitationDemo
+@end
---
+++ zhihu-daily-ImitationDemo/zhihu-daily-ImitationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  zhihu-daily-ImitationDemo
+//
+//  Created by mac on 2018/10/19.
+//  Copyright © 2018 姜凯文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
