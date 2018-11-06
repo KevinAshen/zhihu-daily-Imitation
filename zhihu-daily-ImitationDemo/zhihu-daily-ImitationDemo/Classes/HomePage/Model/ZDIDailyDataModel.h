5e10f60600e0c7d44f2aefca3e6421a888654c20
jiangkaiwen <894912881@qq.com>
2018-11-06 22:32:42 +0800

    完成manager类的建设

--- zhihu-daily-ImitationDemo/zhihu-daily-ImitationDemo/Classes/HomePage/Model/ZDIDailyDataModel.h
+++ zhihu-daily-ImitationDemo/zhihu-daily-ImitationDemo/Classes/HomePage/Model/ZDIDailyDataModel.h
@@ -1,16 +1,16 @@
 //
-//  ZDINavChangeImage.h
+//  ZDIDailyDataModel.h
 //  zhihu-daily-ImitationDemo
 //
-//  Created by mac on 2018/11/2.
+//  Created by mac on 2018/11/6.
 //  Copyright © 2018 姜凯文. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "JSONModel.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ZDINavChangeImage : UIImage
+@interface ZDIDailyDataModel : JSONModel
 
 @end
 
---
+++ zhihu-daily-ImitationDemo/zhihu-daily-ImitationDemo/Classes/HomePage/Model/ZDIDailyDataModel.m
@@ -0,0 +1,13 @@
+//
+//  ZDIDailyDataModel.m
+//  zhihu-daily-ImitationDemo
+//
+//  Created by mac on 2018/11/6.
+//  Copyright © 2018 姜凯文. All rights reserved.
+//
+
+#import "ZDIDailyDataModel.h"
+
+@implementation ZDIDailyDataModel
+
+@end
